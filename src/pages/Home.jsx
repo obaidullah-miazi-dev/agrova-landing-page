c5b1f5c40bacd05c3d03e775cec0c7c5b34b9633
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2026-04-02 07:38:28 +0600

    feat: integrate BrandMarquee and About components into Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Banner from '../components/home/Banner';
+import BrandMarquee from '../components/home/BrandMarquee';
+import About from '../components/home/About';
 
 const Home = () => {
     return (
         <>
           <Banner />  
+          <BrandMarquee />
+          <About />
         </>
     );
 };
