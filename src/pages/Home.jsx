16f7fd075811e27fcd5d64bc7b2124d7b9d36aa0
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2026-04-26 16:05:24 +0600

    feat: integrate Solutions section into Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../components/home/Banner';
 import BrandMarquee from '../components/home/BrandMarquee';
 import About from '../components/home/About';
+import Solutions from '../components/home/Solutions';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
           <Banner />  
           <BrandMarquee />
           <About />
+          <Solutions />
         </>
     );
 };
