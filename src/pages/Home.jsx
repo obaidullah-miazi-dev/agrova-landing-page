5acf339db160652bcb2e7403b01518d0886474d1
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2026-04-26 16:20:32 +0600

    feat: integrate WhyChooseUs section into Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Banner from '../components/home/Banner';
 import BrandMarquee from '../components/home/BrandMarquee';
 import About from '../components/home/About';
 import Solutions from '../components/home/Solutions';
+import WhyChooseUs from '../components/home/WhyChooseUs';
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
           <BrandMarquee />
           <About />
           <Solutions />
+          <WhyChooseUs />
         </>
     );
 };
